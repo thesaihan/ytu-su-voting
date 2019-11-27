72116cf303572f19eae15d3e7c46412c60c52953
Sai Han <thesaihan@gmail.com>
2019-11-27 22:23:27 +0630

    Change backend database from MySQL to SQLite

--- vote/settings.py
+++ vote/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'vote.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'voting',
-        'USER': 'root',
-        'PASSWORD' : 'root',
-        'HOST' : 'localhost',
-        'PORT' :3306
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
